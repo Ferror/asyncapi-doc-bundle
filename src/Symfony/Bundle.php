2e386ad6c522e2c1525cbda6233a15f348fffc77
Ferror <zmalcherczyk@gmail.com>
2023-08-18 00:29:33 +0200

    symfony bundle

---
+++ src/Symfony/Bundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ferror\AsyncapiDocBundle\Symfony;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle as SymfonyBundle;
+
+class Bundle extends SymfonyBundle
+{
+}
