3aca0bf8a1b57d1fc2db919c1bc51efa263d49ac
Ferror <zmalcherczyk@gmail.com>
2024-01-28 22:30:59 +0100

    bug: Documentation Strategy

--- tests/Examples/PaymentExecuted.php
+++ tests/Examples/PaymentExecuted.php
@@ -13,9 +13,7 @@ use Ferror\AsyncapiDocBundle\Schema\PropertyType;
 final readonly class PaymentExecuted
 {
     public function __construct(
-        #[Property(name: 'amount', type: PropertyType::FLOAT, description: 'Payment amount', format: Format::FLOAT, example: '1000')]
         public float $amount,
-        #[Property(name: 'createdAt', type: PropertyType::STRING, description: 'Creation date', format: Format::DATETIME, example: '2023-11-23 13:41:21')]
         public string $createdAt,
     ) {
     }
