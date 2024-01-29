70f232e0f94ba906a90a4dda2e00d58f688e5fc6
Ferror <zmalcherczyk@gmail.com>
2024-01-29 20:40:30 +0100

    chore: minor maintenance

--- tests/Integration/UserInterfaceControllerTest.php
+++ tests/Integration/UserInterfaceControllerTest.php
@@ -7,6 +7,7 @@ namespace Ferror\AsyncapiDocBundle\Tests\Integration;
 use Ferror\AsyncapiDocBundle\Tests\Integration\Service\Kernel;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserInterfaceControllerTest extends TestCase
 {
@@ -14,7 +15,7 @@ class UserInterfaceControllerTest extends TestCase
     {
         $client = new KernelBrowser(new Kernel('test', true));
 
-        $client->request('GET', '/asyncapi');
+        $client->request(Request::METHOD_GET, '/asyncapi');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
